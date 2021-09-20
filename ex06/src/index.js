fc9a583726d9b473e8b425f14f936c8a5945951d
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-20 15:42:43 +0200

    Functions - Returning a Value

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function xSix(num) {
+    return num * 6;
+}
+console.log(5 * 6); // Change this line
+console.log(2 * 6); // Change this line
+console.log(0 * 6); // Change this line
+// Only change code above this line
+
+module.exports = xSix;
