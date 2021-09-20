e67d11b3c148cc92ac402ef5ad39d42c0224aee4
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-20 14:33:00 +0200

    Functions - Local Scope

---
+++ ex04/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+  function localScope() {
+      var myVariable = "I am here!";
+      console.log(myVariable);
+  }
+  localScope();
+  console.log(myVariable);
+// Only change code above this line
+
+// myVariable is not defined outside od localScope
+if(typeof myVariable != "undefined") {
+    console.log("outside localScope", myVariable)
+}  else {
+    console.log("outside localScope UNDEFINED!!!")
+}
+
+module.exports = localScope;
