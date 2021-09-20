93f8bc875e842f8b9348d3300a45f61fa9941645
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-20 18:38:32 +0200

    Izmjena

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,11 +1,13 @@
 // Only change code below this line
 var sum = 0;
 function addThree() {
-    addThree = sum + 3;   
+    addThree = sum + 3;  
+    console.log("addThree: " + addThree);
 }
  addThree(3);
  function addFive() {
      addFive = sum + 5;
+     console.log("addFive: " + addFive);
  }
  addFive(8);
 // Only change code above this line
---
+++ ex08/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+   var total1 = 0;
+   function funcWithArg(digit){
+       
+   }
+
+// Only change code above this line
+console.log(total);
+module.exports = funcWithArg;
