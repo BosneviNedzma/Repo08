63e7885a774df286ce9535c3d2b9ddd19781524e
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-20 17:41:21 +0200

    Functions - Understanding Undefined Value

---
+++ ex07/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var sum = 0;
+function addThree() {
+    addThree = sum + 3;   
+}
+ addThree(3);
+ function addFive() {
+     addFive = sum + 5;
+ }
+ addFive(8);
+// Only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
