60f01250248d17bbbecb6cae527d706267e21034
BosneviNedžma <83877480+BosneviNedzma@users.noreply.github.com>
2021-09-27 09:20:00 +0200

    Update index.js

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,9 +1,7 @@
 // Only change code below this line
    function randomInteger (){
-    Math.floor(Math.random() * 30);
-    return Math.random;}
-
-    randomInteger();
-    console.log(Math.floor(Math.random()* 30));
+    return Math.floor(Math.random() * 30);}
+    
+    console.log(randomInteger());
 // Only change code above this line
 module.exports = randomInteger;
