6240c86c97a4bb03c166b4efa27bead19b75b79a
BosneviNedžma <83877480+BosneviNedzma@users.noreply.github.com>
2021-09-27 08:57:30 +0200

    Update index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -4,6 +4,5 @@ function functionPrintInfo (name, age) {
 
 }
 functionPrintInfo ("John", "23");
-functionPrintInfo ("Michael", "43");
 // Write code above this line
 module.exports = functionPrintInfo;
